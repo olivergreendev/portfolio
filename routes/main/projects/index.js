7a0af259c9b4cf367bf5770ff414695f01d56d43
olivergreendev <olivergreendev@gmail.com>
2021-11-25 20:17:10 +0000

    Updated project showcase with new projects.

--- routes/main/projects/index.js
+++ routes/main/projects/index.js
@@ -3,21 +3,20 @@ const ProjectsRouter = require("express").Router();
 ProjectsRouter.get("/", (req, res) => {
     res.render("projects", { title: "Projects" });
 });
-
 ProjectsRouter.get("/netflix", (req, res) => {
     res.render("netflix", { title: "Netflix Redesign" });
 });
-
 ProjectsRouter.get("/blog", (req, res) => {
     res.render("blog", { title: "Blog" });
 });
-
-ProjectsRouter.get("/movie-search", (req, res) => {
-    res.render("movie-search", { title: "Movie Search" });
+ProjectsRouter.get("/quocosolar", (req, res) => {
+    res.render("quocosolar", { title: "Quocosolar" });
 });
-
-ProjectsRouter.get("/e-commerce", (req, res) => {
-    res.render("e-commerce", { title: "E-Commerce" });
+ProjectsRouter.get("/shoppy", (req, res) => {
+    res.render("shoppy", { title: "Shoppy" });
+});
+ProjectsRouter.get("/yu-gi-oh", (req, res) => {
+    res.render("yu-gi-oh", { title: "Yu-Gi-Oh! Card Search" });
 });
 
 module.exports = ProjectsRouter;
