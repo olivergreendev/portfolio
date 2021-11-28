5f358c9e9964f8d72b7f09b44d042df3478d1caa
olivergreendev <olivergreendev@gmail.com>
2021-11-28 19:28:50 +0000

    Refactoring, removing white space.

--- app.js
+++ app.js
@@ -3,22 +3,17 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const routes = require('./routes/index.js');
-
 // App Variables
 const app = express();
 const port = process.env.PORT || 8000;
-
 // App Configuration
 app.set("views", path.join(__dirname, "views/pages"));
 app.set("view engine", "ejs");
-
 // App Middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "img", "favicon-2.ico")));
-
 // Route Definitions
 require("./routes/index")(app);
-
 // Server Activation
 app.listen(port, () => {
     console.log(`Listening on https:/localhost:${port}`);
