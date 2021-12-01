db80eec82f91a4ba85e1be8950795732d0302c34
olivergreendev <olivergreendev@gmail.com>
2021-12-01 01:14:39 +0000

    Lost track of changes (don't do that again - that's the point of a developer branch, and breaking up commits!)

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-// Required External Modules
+//Required External Modules
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const routes = require('./routes/index.js');
-// App Variables
+//App Variables
 const app = express();
 const port = process.env.PORT || 8000;
-// App Configuration
+//App Configuration
 app.set("views", path.join(__dirname, "views/pages"));
 app.set("view engine", "ejs");
-// App Middleware
+//App Middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "img", "favicon-2.ico")));
-// Route Definitions
+//Route Definitions
 require("./routes/index")(app);
-// Server Activation
+// 404
+app.get('*', function(req, res) {
+    res.status(404).send('404');
+});
+//Server Activation
 app.listen(port, () => {
     console.log(`Listening on https:/localhost:${port}`);
 });
--- routes/main/index.js
+++ routes/main/index.js
@@ -1,11 +1,8 @@
 const IndexRouter = require("express").Router();
-
 IndexRouter.get("/", (req, res) => {
     res.render("index", { title: "Portfolio" });
 });
-
 IndexRouter.get("/contact", (req, res) => {
     res.render("contact", { title: "Contact" });
 });
-
 module.exports = IndexRouter;
